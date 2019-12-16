bc42988c287f1a9d3be3ee66da37bd160dc6ed60
Remigiusz Zudzin <remik.zudzin@gmail.com>
2019-12-16 12:47:03 +0100

    Comparision between getting files and sorting them using LINQ, and not using LINQ.

---
+++ LINQ Fundamentals/Introduction.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Fundamentals
+{
+    /// <summary>
+    /// Writing a program that displays 5 largest files from selected directory without LINQ
+    /// </summary>
+    class Introduction
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\windows";
+            ShowLargeFilesWithoutLinq(path);
+            Console.WriteLine("***");
+            ShowLargeFilesWithLinq(path);
+            Console.ReadKey();
+        }
+
+        private static void ShowLargeFilesWithLinq(string path)
+        {
+            var query = from file in new DirectoryInfo(path).GetFiles()
+                        orderby file.Length descending
+                        select file;
+
+            //or
+            var query2 = new DirectoryInfo(path).GetFiles()
+                                .OrderByDescending(f => f.Length)
+                                .Take(5);
+
+            foreach (var file in query.Take(5))
+            {
+                Console.WriteLine($"{file.Name,-20} : {file.Length,10:N0}");
+            }
+        }
+
+        private static void ShowLargeFilesWithoutLinq(string path)
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);
+            FileInfo[] files = directory.GetFiles();
+            Array.Sort(files, new FileInfocomparer());
+            for (int i = 0; i < 5; i++)
+            {
+                FileInfo file = files[i];
+                Console.WriteLine($"{file.Name, -20} : {file.Length, 10:N0}");
+            }
+        }
+    }
+
+    public class FileInfocomparer : IComparer<FileInfo>
+    {
+        public int Compare(FileInfo x, FileInfo y)
+        {
+            return y.Length.CompareTo(x.Length);
+        }
+    }
+}
