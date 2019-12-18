4120f216bc54a47314a8e6199cd91f4fc2e0cd77
Remigiusz Zudzin <remik.zudzin@gmail.com>
2019-12-18 11:38:18 +0100

    LINQ Queries using methods/or SQLish language

---
+++ Queries/Movie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queries
+{
+    class Movie
+    {
+        public int Amount { get; private set; }
+        public string Title { get; set; }
+        public float Rating { get; set; }
+        int _year;
+        public int Year
+        {
+            get
+            {
+                Amount++;
+                Console.WriteLine($"Returning {_year} for {Title}. In total there are {Amount} instances");
+                return _year;
+            }
+            set
+            {
+                _year = value;
+            }
+        }
+    }
+}
---
+++ Queries/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = MyLinq.Random().Where(n => n < 0.5).Take(10);
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            var movies = new List<Movie>
+            {
+                new Movie { Title = "The Dark Knight", Rating = 8.9f, Year = 2008 },
+                new Movie { Title = "The King's Speech", Rating = 8.0f, Year = 2010 },
+                new Movie { Title = "Casablanca", Rating = 8.5f, Year = 1942 },
+                new Movie { Title = "Star Wars V", Rating = 8.7f, Year = 1980 },
+            };
+
+            var query = Enumerable.Empty<Movie>();
+
+            query = movies.Where(m => m.Year > 2000)
+                          .OrderByDescending(m => m.Rating);
+
+            var query2 = from movie in movies
+                         where movie.Year > 2000
+                         orderby movie.Rating descending
+                         select movie;
+
+            var enumerator = query.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current.Title);
+            }
+
+        }
+    }
+}
